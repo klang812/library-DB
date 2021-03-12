68075001e73e0f062882adfb7544ee6913c0ca87
Kerry Lang <klang812@gmail.com>
2021-03-11 16:25:27 -0800

    add CRUD functionality for books
    Co-authored-by: David Couch <dcouch440@gmail.com>
    
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

--- app.rb
+++ app.rb
@@ -7,10 +7,58 @@ require './lib/book'
 require './databases/db'
 also_reload 'lib/**/*.rb'
 
-tester = {"id" => nil, "title" => 'The shining', "author_id"=> nil, "author" => 'Stephen King', "checked_out" => nil}
+# tester = {"id" => nil, "title" => 'The shining', "author_id"=> nil, "author" => 'Stephen King', "checked_out" => nil}
+
+# tester2 = {"id" => nil, "title" => 'Dogs Best Day', "author_id"=> nil, "author" => 'Stanford Bigsly', "checked_out" => nil}
 
 get('/') do
-  Book.new(tester).save()
   @books = Book.all()
   erb(:home)
 end
+
+
+get('/books') do
+  @books = Book.all()
+  erb(:books)
+end
+
+
+get('/books/:id') do
+  @book = Book.find_book(params[:id])
+  erb(:book)
+end
+
+post('/books') do
+  title = params[:title]
+  author = params[:author]
+  Book.new({"title" =>  title, "author" => author}).save
+  redirect to('/books')
+end
+
+
+delete('/books/:id') do
+  Book.delete(params[:id])
+  redirect to('/books')
+end
+
+
+patch('/books/:id') do
+  @book = Book.find_book(params[:id])
+  @book.update(params)
+  redirect to('/books')
+end
+
+get('/authors') do
+  @authors = Book.find_authors
+  erb(:authors)
+end
+
+get('/authors/:id') do
+  @author = Book.books_by_author(params[:id])
+  erb(:author)
+end
+
+
+
+
+
--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ require_relative 'utils'
 
 class Book
 
-  attr_accessor :checked_out, :id, :author, :author_id, :checked_out
+  attr_accessor :title, :checked_out, :id, :author, :author_id, :checked_out
   def initialize(attr)
     @title = attr["title"]
     @id = attr["id"]
@@ -12,7 +12,7 @@ class Book
   end
 
   def grab_id
-    self.first['id'].to_i()
+    self.first['id']
   end
 
   def set_bool(bool)
@@ -20,32 +20,51 @@ class Book
   end
 
   def self.all
-    all_books = DB.exec("SELECT * FROM books ORDER BY title").first()
+    # the objects are returned
+    all_books = DB.exec("SELECT * FROM books ORDER BY title ASC;")
     books = []
-    [all_books].each do |book|
+    # each iterates through the object somhow "it just knows", we use the data to set the values of all availible data and return instances in an array
+    all_books.each do |book|
       id = book["id"]
       title = book["title"]
-      author = DB.exec("Select author FROM authors WHERE id = #{ book.author_id }")
-      author_id = book["author_id"].to_i
+      author = DB.exec("Select author FROM authors WHERE id = #{ book["author_id"] }").first["author"]
+      author_id = book["author_id"]
       checked_out = book["checked_out"]
-      books.push(Book.new({"id" => id, "title" => title, "author" => author, "author_id" => author_id, "checked_out" => checked_out}))
+      books.push(Book.new(
+          {
+            "id" => id,
+            "title" => title,
+            "author" => author,
+            "author_id" => author_id,
+            "checked_out" => checked_out
+          }
+      ))
     end
     books
   end
 
   def save
+    # is there authors?
     @author_id = DB.exec(
       "SELECT id FROM authors WHERE lower(author) = ('#{ @author.downcase }');"
-    ).first['id'].to_i()
+    )
 
-    if !@author_id
-      @author_id = DB.exec("INSERT INTO authors (author) VALUES ('#{ @author }') RETURNING id;").first['id'].to_i()
+    # nope? ok moke one and give me that id
+    if !@author_id.first()
+      @author_id = DB.exec(
+        "INSERT INTO authors (author) VALUES ('#{ @author }') RETURNING id;"
+      )
     end
 
-    @return_values = DB.exec("INSERT INTO books (title, author_id) VALUES ('#{ @title }', '#{ @author_id }') RETURNING id, checked_out;").first()
-
+    # use that info to save that id in our object
+    # {"id" => nil, "title" => 'Dogs Best Day', "author_id"=> nil, "author" => 'Stanford Bigsly', "checked_out" => nil}
+    @return_values = DB.exec("INSERT INTO books (title, author_id) VALUES ('#{ @title }', '#{ @author_id.first["id"] }') RETURNING id, checked_out;").first()
+    # use that values to give me usable data
     @id = @return_values["id"]
     @checked_out = set_bool(@return_values["checked_out"])
+
+    # instance works how i want it.
+    self
   end
 
   def grab_id
@@ -58,19 +77,25 @@ class Book
   end
 
   def self.find_book( book_id )
+    # generates an instance from the id
     book = DB.exec("SELECT * FROM books WHERE id = #{ book_id };").first()
-    author = DB.exec("Select author FROM authors WHERE id = #{ book.author_id }").first()
-    book["author"] = author["author"]
-    Book.new(book)
+    if book
+      # notice how we make a second call to match the authors name in our class and now have that value
+      author = DB.exec("Select author FROM authors WHERE id = #{ book["author_id"] }").first()
+      book["author"] = author["author"]
+      Book.new(book)
+    else
+      nil
+    end
   end
 
   def update(attributes)
-    if (attributes.has_key?(:title)) && (attributes.fetch(:title) != nil)
-      @title = attributes.fetch(:title)
+    if (attributes.has_key?("title")) && (attributes.fetch("title") != '')
+      @title = attributes.fetch("title")
       DB.exec("UPDATE books SET title = '#{@title}' WHERE id = #{@id};")
     end
-    if (attributes.has_key?(:author)) && (attributes.fetch(:author) != nil)
-      @author = attributes.fetch(:author)
+    if (attributes.has_key?("author")) && (attributes.fetch("author") != '')
+      @author = attributes.fetch("author")
       DB.exec("UPDATE authors SET author = '#{@author}' WHERE id = #{@author_id};")
     end
   end
@@ -79,5 +104,26 @@ class Book
     DB.exec("DELETE FROM books WHERE id = #{ book_id };")
   end
 
+  def self.find_authors(author_id: nil, author: nil)
+    if author_id
+      DB.exec("SELECT * FROM authors WHERE id = #{author_id};")
+    elsif author
+      DB.exec("SELECT * FROM authors WHERE lower(author) = #{author.downcase};")
+    else
+      DB.exec("SELECT * FROM authors;")
+    end
+  end
+
+  def self.books_by_author ( author_id )
+    books = []
+    results = DB.exec("SELECT * FROM books WHERE author_id = #{author_id};")
+    results.each do |result|
+      book = DB.exec("SELECT * FROM books WHERE id = #{id};")
+      author = DB.exec("SELECT author FROM authors WHERE id = #{author_id}").first()
+      result["author_id"] = author["author_id"]
+      books.push(Book.new(result))
+    end
+    books
+  end
 end
 
